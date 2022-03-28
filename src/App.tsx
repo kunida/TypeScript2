874575aa125c8c2ae64632cbbf24b093ab8a4703
kunida <67056950+kunida@users.noreply.github.com>
2022-03-28 21:30:56 +0900

    オプショナルチェイニング

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import { Todo } from "./Todo";
 import "./styles.css";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "くったん",
+  hobbies: ["ゲーム", "キャンプ"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -18,6 +25,7 @@ export default function App() {
 
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
